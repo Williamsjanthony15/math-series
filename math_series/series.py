49f1bf7e68066a0d3cb255eb5c4d89a381dc0e45
Anthony Williams <Williamsjanthony15@yahoo.com>
2021-06-09 23:05:23 -0500

    finished, all tests passing

--- math_series/series.py
+++ math_series/series.py
@@ -26,4 +26,9 @@ def sum_series(n, first = 0, second = 1):
   """ 
     Sum Series for lab2. This will be my sum series, version of the equation. First argument is required, the second two are optional. First param will determine which element in the series to print. The two optional params will have default values of 0 and 1, and will determine the first two values for the series to be produced. 
   """ 
-  pass 
+  # pass
+  seq_list = [first, second]
+  for i in range (2, n + 1):
+      next_n = seq_list[-1] + seq_list[-2]
+      seq_list.append(next_n)
+  return seq_list[n - 1]
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,35 +1,35 @@
 from math_series import __version__
-from math_series.series import fibonacci, lucas
+from math_series.series import fibonacci, lucas, sum_series
 
 def test_version():
   assert __version__ == '0.1.0'
 
-def fourTestFibo():
+def test_fourTestFibo():
     actual = fibonacci(4)
     expected = 2
     assert actual == expected
 
-def eightTestFibo():
+def test_eightTestFibo():
     actual = fibonacci(8)
     expected = 13
     assert actual == expected
 
-def fourTestLucas():
+def test_fourTestLucas():
     actual = lucas(4)
     expected = 4
     assert actual == expected 
 
-def eightTestLucas():
+def test_eightTestLucas():
     actual = lucas(8)
     expected = 29
     assert actual == expected 
 
-def fourSeriesSum():
-    actual = fourSeriesSum(4)
+def test_fourSeriesSum():
+    actual = sum_series(4)
     expected = 2
     assert actual == expected
 
-def eightSeriesSum():
-    actual = eightSeriesSum(8)
-    expected = 4
+def test_eightSeriesSum():
+    actual = sum_series(8)
+    expected = 13
     assert actual == expected
